f999ece7cc4d51547f1e2b97dbbf151a292a4edc
董情情 <dongqingqing@weimai.com>
2017-08-27 17:43:36 +0800

    init project , add algs4 and book-algorithm4rd and personal-practice module

---
+++ book-algorithm4rd/src/main/java/com/group/algorithm/algorithm4rd/chapter1_1/Exercise08_dongqingqing.java
@@ -0,0 +1,15 @@
+package com.group.algorithm.algorithm4rd.chapter1_1;
+
+/**
+ * Created by qqdong on 2017/8/27.
+ * 下列语句会打印什么结果
+ */
+public class Exercise08_dongqingqing {
+    public static void main(String[] args) {
+        printTestSentence();
+    }
+
+    private static void printTestSentence() {
+
+    }
+}
---
+++ personal-practice/src/main/java/com/group/algroithm/personal/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by qqdong on 2017/8/27.
+ */
+package com.group.algroithm.personal;
